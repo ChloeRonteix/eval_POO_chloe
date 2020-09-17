ab4d0349f30e27b7689d865715c7cfaf5a15022d
ChloeRonteix <59954097+ChloeRonteix@users.noreply.github.com>
2020-09-17 15:42:48 +0200

    add_note

--- note.py
+++ note.py
@@ -5,6 +5,10 @@ class Note:
     def __init__(self):
         self.value = int
 
+
+    def add_note(self, note):
+        pass
+
     @classmethod
     def find(cls, value) -> List(value):
         pass
